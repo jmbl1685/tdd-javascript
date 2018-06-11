0236fdfb7a7814bc6071b9aaaef87b2d09c6240f
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2018-06-11 11:32:08 -0500

    JB - Adding [chai-http] Mocha Test

---
+++ server/server.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.post('/mytest', (req, res) => {
+
+    const { number, vowel } = req.body
+
+    if (number === undefined || vowel === undefined)
+        return res.status(404).send({ error: 'missing fields' })
+
+    return res.status(200).send({ number, vowel })
+})
+
+app.listen(3000, () => {
+    console.log('Server running')
+})
+
+module.exports = app
--- test/mocha.test.js
+++ test/mocha.test.js
@@ -1,16 +1,28 @@
 'use strict'
 
 const chai = require('chai')
+const server = require('../server/server')
 const _function = require('../functions')
+
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+let should = chai.should()
 
 describe('Unit tests using Mocha', () => {
 
-  it('isNumber', () => {
-    chai.expect(_function.isNumber(2)).to.equal('number')
-  })
+  it('isNumber and isVowel', () => {
 
-  it('isVowel', () => {
-    chai.expect(_function.isVowel('a')).to.equal(true)
+    chai.request(server)
+      .post('/mytest')
+      .send({ number: 5, vowel: 'a'})
+      .end((err,res) => {
+        const { number, vowel } = res.body
+        chai.expect(_function.isNumber(number)).to.equal('number')
+        chai.expect(_function.isVowel(vowel)).to.equal(true)
+        res.should.have.status(200)    
+    })
+
   })
 
 })
